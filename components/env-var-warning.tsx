6a9e8abc00b6c70eeecbdf27c98d1e3a2e775bf2
vinayakprwl <vinayak.p@teamwebmark.com>
2025-02-02 14:11:06 +0530

    third commmit

--- components/env-var-warning.tsx
+++ components/env-var-warning.tsx
@@ -1,13 +1,12 @@
 import Link from "next/link";
-import { Badge } from "./ui/badge";
 import { Button } from "./ui/button";
 
 export function EnvVarWarning() {
   return (
     <div className="flex gap-4 items-center">
-      <Badge variant={"outline"} className="font-normal">
+      <div className="font-normal">
         Supabase environment variables required
-      </Badge>
+      </div>
       <div className="flex gap-2">
         <Button
           asChild
